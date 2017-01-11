cc09c464cf9e9b8b2e921d5e236e1818280cf8db
Samadhan <samtestonline@gmail.comm>
2017-01-11 15:06:11 +0530

    Initial commit

---
+++ SKCustomClass+Extensions/SKCustomClass+Extensions.h
@@ -0,0 +1,19 @@
+//
+//  SKCustomClass+Extensions.h
+//  SKCustomClass+Extensions
+//
+//  Created by steve jobs on 11/01/17.
+//  Copyright © 2017 samKolhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKCustomClass+Extensions.
+FOUNDATION_EXPORT double SKCustomClass_ExtensionsVersionNumber;
+
+//! Project version string for SKCustomClass+Extensions.
+FOUNDATION_EXPORT const unsigned char SKCustomClass_ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKCustomClass_Extensions/PublicHeader.h>
+
+
